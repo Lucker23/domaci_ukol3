abf84a494b107c97542ee35324ca91033a4be409
Yuriy <kupinskyy.yuriy@gmail.com>
2024-03-14 19:42:50 +0100

    ukoly hotovy

--- src/components/Option/index.jsx
+++ src/components/Option/index.jsx
@@ -1,11 +1,17 @@
 import { Icon } from '../Icon/index';
 import './style.css';
 
-export const Option = (props) => {
+export const Option = ({type, text, onSelected }) => {
+  const handleClick = () => {
+    if (onSelected) {
+      onSelected(type);
+    }
+  };
+
   return (
-    <div className="option">
-      <Icon type={props.type} />
-      <div>{props.text}</div>
+    <div className="option" onClick={handleClick}>
+      <Icon type={type} />
+      <div>{text}</div>
     </div>
   );
 };
--- src/components/Question/index.jsx
+++ src/components/Question/index.jsx
@@ -1,20 +1,26 @@
 import { QuestionBody } from '../QuestionBody/index';
 import { Option } from '../Option/index';
 import './style.css';
+import { useState } from 'react';
 
-export const Question = (props) => {
+export const Question = ({text}) => {
+  const [answer, setAnswer] = useState("symbolQuestion");
+
+  const handleAnswer = (iconType) => {
+    setAnswer(iconType) 
+  }
   return (
     <div className="question">
       <QuestionBody 
-        iconType="symbolQuestion" 
-        text={props.text}
+        iconType={answer} 
+        text={text}
       />
       <div className="question__options">
-        <Option type="smileyStrongYes" text="Souhlasím"/>
-        <Option type="smileyYes" text="Spíše souhlasím"/>
-        <Option type="smileyNeutral" text="Nevím"/>
-        <Option type="smileyNo" text="Spíše nesouhlasím"/>
-        <Option type="smileyStrongNo" text="Nesouhlasím"/>
+        <Option type="smileyStrongYes" text="Souhlasím" onSelected={handleAnswer}/>
+        <Option type="smileyYes" text="Spíše souhlasím" onSelected={handleAnswer}/>
+        <Option type="smileyNeutral" text="Nevím" onSelected={handleAnswer}/>
+        <Option type="smileyNo" text="Spíše nesouhlasím" onSelected={handleAnswer}/>
+        <Option type="smileyStrongNo" text="Nesouhlasím" onSelected={handleAnswer}/>
       </div>
     </div>
   );
